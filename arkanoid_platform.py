1d56861677defe6011c5b67960afccbe398a50cb
KalininSaveliy <kalinin.savelii@phystech.edu>
2019-11-27 20:05:24 +0300

    Модуль платформы доведен до минимального уровня использования. Не согласен? Звони

--- arkanoid_platform.py
+++ arkanoid_platform.py
@@ -1,4 +1,10 @@
-"""Модуль, отвечающий за движение платформы"""
+"""Модуль, отвечающий за класс платформы
+
+    Получает на вход: полотно для рисования,
+                      размеры полотна
+
+    Модуль может: передвигать платформу по нажатию клавиш A и D (Left и Right)
+                  выводить координаты платформы"""
 
 
 class Platform:
@@ -7,11 +13,17 @@ class Platform:
         self.length = 70  # длина платформы
         self.thickness = 7  # толщина платформы
         self.color = 'blue'
+
+        # координаты ценрта платформы
         self.x = window_width / 2
         self.y = window_height * 7 / 8
+
+        # диапазон движения центра платформы
+        self.x_max = window_width - self.length / 2
+        self.x_min = self.length / 2
         self.canvas = canvas
 
-        # создаем платформу
+        # создаем платформу, в этом разделе задается ее форма
         self.platform = [canvas.create_oval(self.x - (self.length - self.thickness) / 2, self.y - self.thickness / 2,
                                             self.x - (self.length + self.thickness) / 2, self.y + self.thickness / 2,
                                             fill=self.color),
@@ -20,28 +32,39 @@ class Platform:
                                             self.x - (- self.length + self.thickness) / 2, self.y + self.thickness / 2,
                                             fill=self.color),
 
-                         canvas.create_rectangle(self.x - self.length / 2, self.y - self.thickness / 2,
-                                                 self.x + self.length / 2, self.y + self.thickness / 2,
-                                                 fill=self.color)]
-        #            canvas.create_rectangle(canvas.create_oval(x - (length - d) / 2, y - d / 2,
-        #                                                       x - (length + d) / 2, y + d / 2,
-        #                                                       fill=color))
-        # фича: рисует красиво, но выдает ошибку
+                         canvas.create_oval(self.x - self.length / 2, self.y - self.thickness / 2,
+                                            self.x + self.length / 2, self.y + self.thickness / 2,
+                                            fill=self.color)]
 
     def platform_centre(self):
-        return self.x, self.y - self.thickness, self.l
+        """Выводит координаты в формате: х центра платформы и у его верхней грани"""
+        return self.x, self.y - self.thickness, self.length
 
-    def move_platform(self, shift):
-        for part in self.platform:
-            self.canvas.move(part, shift, 0)
+    def move_platform(self, event):
+        """Передвижение платформы при нажатии клавиш"""
+        shift = 30  # модуль сдвига платформы за одно нажатие клавишы
 
-    def shift_platform(self, event):
-        shift = 20
-        if event.keysym == 'a' or 'Left':
-            shift = - shift
-        if event.kesym == 'd' or 'Right':
-            pass
-            S
+        # перемещение вправо
+        if event.keysym == 'd' or event.keysym == 'Right':
+            if self.x <= self.x_max - shift:
+                self.x += shift
+                for part in self.platform:
+                    self.canvas.move(part, shift, 0)
+            elif self.x <= self.x_max:
+                self.x = self.x_max
+                for part in self.platform:
+                    self.canvas.move(part, self.x_max - self.x, 0)
+
+        # перемещение влево
+        if event.keysym == 'a' or event.keysym == 'Left':
+            if self.x >= self.x_min + shift:
+                self.x -= shift
+                for part in self.platform:
+                    self.canvas.move(part, -shift, 0)
+            elif self.x >= self.x_min:
+                self.x = self.x_min
+                for part in self.platform:
+                    self.canvas.move(part, self.x_min - self.x, 0)
 
 
 if __name__ == "__main__":
