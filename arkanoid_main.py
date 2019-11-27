28fa31a8c39c9a78d551f85cc72aa3ce94343b41
KalininSaveliy <kalinin.savelii@phystech.edu>
2019-11-27 11:21:51 +0300

    Платформа + ее жвижение

--- arkanoid_main.py
+++ arkanoid_main.py
@@ -7,20 +7,23 @@ from arkanoid_platform import *
 
 
 def main():
-    global canvas
+    global canvas, root, window_width, window_height
     window_width = 800
     window_height = 600
     # создаем окно для игры
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
     canvas.pack(side=tkinter.TOP)
-    canvas.bind('<Double-Button-1>', new_game)  # запуск игры двойным щелчком
+    root.bind('<Double-Button-1>', new_game)  # запуск игры двойным щелчком
     root.mainloop()
 
 
 def new_game(event):
     score = 0
-    text_score = canvas.create_text(40, 25, text='Score: ' + str(score), fill='white')
+    text_score = canvas.create_text(window_width/20, window_height/24,
+                                    text='Score: ' + str(score), fill='white')
+    platform = Platform(canvas, window_width, window_height)
+    root.bind('<Key>', platform.move_platform)
 
 
 main()
--- arkanoid_platform.py
+++ arkanoid_platform.py
@@ -1,3 +1,48 @@
 """Модуль, отвечающий за движение платформы"""
 
-pass
+
+class Platform:
+
+    def __init__(self, canvas, window_width, window_height):
+        self.length = 70  # длина платформы
+        self.thickness = 7  # толщина платформы
+        self.color = 'blue'
+        self.x = window_width / 2
+        self.y = window_height * 7 / 8
+        self.canvas = canvas
+
+        # создаем платформу
+        self.platform = [canvas.create_oval(self.x - (self.length - self.thickness) / 2, self.y - self.thickness / 2,
+                                            self.x - (self.length + self.thickness) / 2, self.y + self.thickness / 2,
+                                            fill=self.color),
+
+                         canvas.create_oval(self.x - (- self.length - self.thickness) / 2, self.y - self.thickness / 2,
+                                            self.x - (- self.length + self.thickness) / 2, self.y + self.thickness / 2,
+                                            fill=self.color),
+
+                         canvas.create_rectangle(self.x - self.length / 2, self.y - self.thickness / 2,
+                                                 self.x + self.length / 2, self.y + self.thickness / 2,
+                                                 fill=self.color)]
+        #            canvas.create_rectangle(canvas.create_oval(x - (length - d) / 2, y - d / 2,
+        #                                                       x - (length + d) / 2, y + d / 2,
+        #                                                       fill=color))
+        # фича: рисует красиво, но выдает ошибку
+
+    def platform_centre(self):
+        return self.x, self.y - self.thickness, self.l
+
+    def move_platform(self, shift):
+        for part in self.platform:
+            self.canvas.move(part, shift, 0)
+
+    def shift_platform(self, event):
+        shift = 20
+        if event.keysym == 'a' or 'Left':
+            shift = - shift
+        if event.kesym == 'd' or 'Right':
+            pass
+            S
+
+
+if __name__ == "__main__":
+    print('This module is not for direct call')
