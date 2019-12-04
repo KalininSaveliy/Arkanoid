9089e30c92182c3b6f44f0da9778eb588dfc298c
KalininSaveliy <kalinin.savelii@phystech.edu>
2019-12-04 10:21:25 +0300

    Что-то поменял

--- arkanoid_ball.py
+++ arkanoid_ball.py
@@ -1,39 +1,40 @@
 """Модуль, отвечающий за движение и соударение шарика"""
 
-v = 11
-colors = ['red','orange','yellow','green','blue']
 
-class Ball(canv, platform):
-    
-    def __init__(self):
-        self.r     = 5
-        self.x     = platform.x
-        self.y     = platform.y + self.r
-        self.dx    = rnd(-10,10)
-        self.dy    = (v**2-dx**2)**0.5
-        self.ball  = canv.create_oval(self.x - self.r,self.y - self.r, self.x + self.r, self.y + self.r)
+from random import choice, randint as rnd
+
+
+class Ball:
+    def __init__(self, canvas, platform):
+        self.canvas = canvas
+        self.v = 11
+        colors = ['red', 'orange', 'yellow', 'green', 'blue']
+        self.r = 5
+        self.x, self.y, self.platform_len = platform.centre
+        self.y -= self.r
+        self.dx = rnd(-10, 10)
+        self.dy = - (self.v**2 - self.dx**2)**0.5
         self.color = choice(colors)
-        #canv.itemconfig(self.ball, fill="red") # не шарю
-        #canv.addtag_withtag("ball", self.ball) # не шарю
-
-    "Функция, отвечающая за движение шарика"
+        self.ball = canvas.create_oval(self.x - self.r,self.y - self.r,
+                                       self.x + self.r, self.y + self.r,
+                                       fill=self.color)
+        #canv.itemconfig(self.ball, fill="red")  # не шарю
+        #canv.addtag_withtag("ball", self.ball)  # не шарю
     
     def move_ball(self):
+        """Функция, отвечающая за движение шарика"""
+        if True:  # провека условия соударения шарика с блоками или платформой
+            pass
+        self.x += self.dx
+        self.y += self.dy
+        self.canvas.move(self.ball, self.dx, self.dy)
     
-        canv.move(ball,dx,dy)
-        x+=dx
-        y+=dy
-        canv.move(self.ball, self.dx, self.dy)
-    
-    "Функция, отвечающая за соударение шарика"
-    
-    def rebound_ball(self):
-        
-        if  y <= r or y >= window_height - r:
-            dy=-dy
-        if  x <= r or x >= window_width - r:
-            dx=-dx
-        if self.x < h_wall.x + h_wall.width and self.x + self.r > h_wall.x and self.y < h_wall.y + h_wall.height and self.y + self.r > h_wall.y:
-            # нужно увеличивать счётчик очков
+    # def rebound_ball(self):
+    #     """Функция, отвечающая за соударение шарика"""
+    #     if  self.y <= self.r or self.y >= window_height - self.r:
+    #         self.dy=-self.dy
+    #     if  self.x <= self.r or self.x >= self.window_width - self.r:
+    #         self.dx=-self.dx
+    #     if self.x < h_wall.x + h_wall.width and self.x + self.r > h_wall.x and self.y < h_wall.y + h_wall.height and self.y + self.r > h_wall.y:
+            # нужно увеличивать счётчик очков для этого есть переменная score
             # нужно прописать отскок в этом случае (есть отскок сверху снизу и слева справа)
-pass
--- arkanoid_main.py
+++ arkanoid_main.py
@@ -3,28 +3,35 @@
 import tkinter
 from arkanoid_level import *
 from arkanoid_block import *
-# from arkanoid_ball import *
+from arkanoid_ball import *
 from arkanoid_platform import *
 
 
+
+bind_id = None
+# создаем окно для игры
+window_width = 800
+window_height = 600
+root = tkinter.Tk()
+canvas = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
+canvas.pack(side=tkinter.TOP)
+is_running = False
+
+
 def main():
-    global canvas, root, window_width, window_height
-    window_width = 800
-    window_height = 600
-    # создаем окно для игры
-    root = tkinter.Tk()
-    canvas = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
-    canvas.pack(side=tkinter.TOP)
     root.bind('<Double-Button-1>', new_game)  # запуск игры двойным щелчком
     root.mainloop()
 
 
 def new_game(event):
+    global is_running
+    if is_running:
+        return
+    is_running = True
     score = 0
     text_score = canvas.create_text(window_width/20, window_height/24,
                                     text='Score: ' + str(score), fill='white')
     platform = Platform(canvas, window_width, window_height)
-
     # создание чек-уровня, в переменную blocks записываються данные о блоках
     blocks = create_check_level(canvas, window_width, window_height)
     root.bind('<Key>', platform.move_platform)
