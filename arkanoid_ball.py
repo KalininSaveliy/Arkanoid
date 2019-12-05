d0bdbb76c2fd08c469a38b9f04081e64bafaf279
pavmesh <petya.123007@yandex.ru>
2019-12-06 02:51:26 +0300

    пока странно работающая физика

--- arkanoid_ball.py
+++ arkanoid_ball.py
@@ -1,114 +1,34 @@
 """Модуль, отвечающий за движение и соударение шарика"""
-from random import randint, choice 
-v = 10
+from random import randint, choice
+import math
 colors = ['red','orange','yellow','green','blue']
 
 class Ball:
-    
-    def __init__(self, canvas, x, y, r, window_width, window_height):
-        self.width = window_width
-        self.height = window_height
+    def __init__(self, canvas, x, y, r, max_speed):
 
-        self.c = canvas
+        self.canvas = canvas
 
-        self.r = r
         self.x = x
         self.y = y
+        self.r = r
 
-        self.dx    = randint(-10,10)
-        self.dy    = (v**2-self.dx**2)**0.5
 
-        self.ball  = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+        self.dx    = randint(-max_speed, max_speed)
+        self.dy    = - (max_speed ** 2 - self.dx ** 2) ** 0.5
+
+        # canvas drawing
+        self.ball  = canvas.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
         self.color = choice(colors)
-        canv.itemconfig(self.ball, fill=self.color)
+        self.canvas.itemconfig(self.ball, fill=self.color)
         #canv.addtag_withtag("ball", self.ball) # не шарю
 
-    # change color func
-
-    "Функция, отвечающая за движение шарика"
-
-    def check_collision_x(self, obj):
-        return obj.x - self.r <= self.x + self.dx and self.x + self.dx <= obj.x + obj.w + self.r
-
-    def check_collision_y(self, obj):
-        return obj.y - self.r <= self.y + self.dy and self.y + self.dy <= obj.y + obj.h + self.r
-
-    def move(self, blocks, platform):
-
-        def lengthhh(x, y):
-            return (x**2 + y**2) ** 0.5
-
-        def dot(x1, y1, x2, y2):
-            return x1 * y1 + x2 * y2
-
-        def cross(x1, y1, x2, y2):
-            return x1 * y2 - x2 * y1
-
-        def check_collide(bx, by, x0, y0, x1, y1, x2, y2):
-            #check area
-            area = cross(x0 - bx, y0 - by, x2 - bx, y2 - by)
-            # dist from bx,by to segment x0,y0 - x2,y2
-            dist = area / lengthhh(x2-x0, y2-x0)
-
-            if dist > lengthhh(x1-bx, y1-by):
-                return False
-
-            return cross(x0-bx, y0-by, x1-bx, y1-by) * cross(x1-bx, y1-by, x2-bx, y2-by) > 0
 
 
 
-        # global v
-        # length = d
 
-        # # шаг вектор
-        # step_dx = self.dx / v
-        # step_dy = self.dy / v
+    "Перемещение на dx,dy"
 
-        # # сохранение изменённой координаты вектора
-        # nx = self.x
-        # ny = self.y
-
-        # # while length >= 1:
-        #   for b in blocks:
-        #     if check_collide(nx, ny, b.x, b.y, step_dx, step_dy, b.x, b.y + b.height):
-
-        
-
-        for b in blocks:
-            if check_collide(self.x, self.y, b.x + b.width, b.y, self.dx, self.dy, b.x, b.y):
-                self.dy = -self.dy
-            elif check_collide(self.x, self.y, b.x, b.y + b.height, self.dx, self.dy, b.x, b.y):
-                self.dx = -self.dx
-            elif check_collide(self.x, self.y, b.x + b.width, b.y + b.height, self.dx, self.dy, b.x, b.y + b.height):
-                self.dy = -self.dy
-            elif check_collide(self.x, self.y, b.x, b.y + b.height, self.dx, self.dy, b.x + b.width, b.y):
-                self.dx = -self.dx
-
-        if check_collide(self.x, self.y, 0, 0, self.dx, self.dy, self.width, 0):
-            self.dy = -self.dy
-        
-        elif check_collide(self.x, self.y, 0, 0, self.dy, self.dy, 0, self.height):
-            self.dx = -self.dx
-
-        elif check_collide(self.x, self.y, self.width, 0, self.dx, self.dy, self.width, self.height):
-            self.dx = -self.dx
-
-        elif check_collide(self.x, self.y, 0, self.height, self.dx, self.dy, self.width, self.height):
-            self.dy = -self.dy
-    
+    def move(self):
         self.x += self.dx
         self.y += self.dy
-        print(self.x, self.y)
-        self.canv.move(self.ball, self.dx, self.dy)
-    
-    "Функция, отвечающая за соударение шарика"
-    
-    def check_boundaries(self):
-        if  self.y <= self.r or self.y >= self.height - self.r:
-            self.dy= -self.dy
-        if  self.x <= r or x >= window_width - r:
-            dx=-dx
-        if self.x < h_wall.x + h_wall.width and self.x + self.r > h_wall.x and self.y < h_wall.y + h_wall.height and self.y + self.r > h_wall.y:
-            # нужно увеличивать счётчик очков
-            # нужно прописать отскок в этом случае (есть отскок сверху снизу и слева справа)
-            pass
+        self.canvas.move(self.ball, self.dx, self.dy)
--- arkanoid_main.py
+++ arkanoid_main.py
@@ -59,74 +59,149 @@ class Game():
         self.score = 0
         self.score_element = self.canvas.create_text(self.width / 20, self.height / 24, text = "Score: " + str(self.score), fill = "black")
 
+        self.debug_line = self.canvas.create_line(0,0,1,1)  
+
         # draw start screen
         self.root.bind("<Double-Button-1>", self.start_new_game)
         self.root.mainloop()
 
-    # def check_point_in_box(self, x, y, box):
-    #     if 9 
-    # #write test collision
+
+    def dist_if_collide(self, bx, by, rx, ry, fx, fy, sx, sy):
+        def vec2(bx, by, x, y):
+            return [x - bx, y - by]
+
+        def dist(v):
+            return (v[0] ** 2 + v[1] ** 2) ** 0.5
+
+        def cross(f, s):
+            return f[0] * s[1] - s[0] * f[1]
+
+        first  = vec2(bx, by, fx, fy)
+        second = vec2(bx, by, sx, sy)
+        ray    = vec2(bx, by, rx, ry)
+
+        first_area  = cross(first, ray)
+        second_area = cross(ray, second)
+        area        = cross(first, second)
+
+        do_calc_dist = False
+
+        # проверяем пересечение
+        if area > 0:
+            if first_area > 0 and second_area > 0:
+                do_calc_dist = True
+        elif area < 0:
+            if first_area < 0 and second_area < 0:
+                do_calc_dist = True
+        elif first_area == 0:
+                return min(dist(vec2(ray[0], ray[1], first[0], first[1])), dist(vec2(ray[0], ray[1], second[0], second[1])))
+
+        if do_calc_dist:
+            return area / (first_area + second_area)
+            self.canvas.create_line(x1, y1, x2, y2, fill="orange")
+        else:
+            return math.inf
 
     def gamecycle(self):
-        # is_x_detected = False
-        # is_y_detected = False
+
+        self.r = math.inf
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+
+        def process_rect(x, y, w, h):
+            # global r, x1, x2, y1, y2 # wtf didn't work but must. just trying to access gamecycle variables ...
+
+            def process_line(x1, y1, x2, y2):
+
+                ndist = self.dist_if_collide(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, x1, y1, x2, y2)
+                if self.r > ndist:
+                    self.x1 = x1
+                    self.y1 = y1
+                    self.x2 = x2
+                    self.y2 = y2
+                    self.r = ndist
+
+            process_line(x, y, x+w, y)
+            process_line(x, y, x, y+h)
+            process_line(x+w, y+h, x+w, y)
+            process_line(x+w, y+h, x, y+h)
+            # def process_line(x, y, w, h)
+            # ndist = self.dist_if_collide(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, x - self.ball.r, y - self.ball.r, x + w + self.ball.r, y - self.ball.r)
+            # if self.r > ndist:
+            #     self.x1 = x - self.ball.r
+            #     self.y1 = y - self.ball.r
+            #     self.x2 = x + w + self.ball.r
+            #     self.y2 = y - self.ball.r
+            #     self.r = ndist
+
+            # # process_line(x - self.ball.r / 2, y - self.ball.r / 2, x + w + self.ball.r / 2, y - self.ball.r / 2)
+            # # process_line(x - self.ball.r / 2, y - self.ball.r / 2, x - self.ball.r / 2, y + h + self.ball.r / 2)
+            # # process_line(x + w + self.ball.r / 2, y + h + self.ball.r / 2, x + w + self.ball.r / 2, y - self.ball.r / 2)
+            # # process_line(x + w + self.ball.r / 2, y + h + self.ball.r / 2, x - self.ball.r / 2, y + h + self.ball.r / 2)
+
+            # ndist = self.dist_if_collide(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, x - self.ball.r, y - self.ball.r, x - self.ball.r, y + h + self.ball.r)
+            # if self.r > ndist:
+            #     self.x1 = x - self.ball.r
+            #     self.y1 = y - self.ball.r
+            #     self.x2 = x - self.ball.r
+            #     self.y2 = y + h + self.ball.r
+            #     self.r = ndist
+
+            # ndist = self.dist_if_collide(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, x + w + self.ball.r, y + h + self.ball.r, x + w + self.ball.r, y - self.ball.r)
+            # if self.r > ndist:
+            #     self.x1 = x + w + self.ball.r
+            #     self.y1 = y + h + self.ball.r
+            #     self.x2 = x + w + self.ball.r
+            #     self.y2 = y - self.ball.r
+            #     self.r = ndist
+
+            # ndist = self.dist_if_collide(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, x + w + self.ball.r, y + h + self.ball.r, x - self.ball.r, y + h + self.ball.r)
+            # if self.r > ndist:
+            #     self.x1 = x + w + self.ball.r
+            #     self.y1 = y + h + self.ball.r
+            #     self.x2 = x - self.ball.r
+            #     self.y2 = y + h + self.ball.r
+            #     self.r = ndist
+
 
         # # check blocks collisions
-        # for b in self.blocks:
+        for b in self.blocks:
+            process_rect(b.x, b.y, b.length, b.thickness)
 
-        #     class block_wrap:
-        #         x = b.x
-        #         y = b.y
-        #         w = b.length
-        #         h = b.thickness
+        # platform collision check
+        process_rect(self.platform.x - self.platform.length / 2, self.platform.y - self.platform.thickness / 2, self.platform.length, self.platform.thickness)
 
-        #     x_check =
+        # boundaries check
+        process_rect(0, 0, self.width, self.height)
 
-        #     if self.ball.check_collision_x(block_wrap):
-        #         is_x_detected = True
+        print(self.ball.x, self.ball.y, self.ball.dx, self.ball.dy, 1/self.r)
+        self.canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill="white")
+        
+        if self.r > 1:
+            oldx = self.ball.x
+            oldy = self.ball.y
 
-        #     if self.ball.check_collision_y(block_wrap):
-        #         is_y_detected = True
+            if self.y2 - self.y1 == 0: # cross [1, 0], [x2-x1, y2-y1]
+                self.ball.x += self.ball.dx
+                self.ball.y += 1 / self.r * self.ball.dy - (1 - 1/self.r) * self.ball.dy
+                self.ball.dy *= -1
+            else:
+                self.ball.y += self.ball.dy
+                self.ball.x += 1 / self.r * self.ball.dx - (1 - 1/self.r) * self.ball.dx
+                self.ball.dx *= -1
+            self.canvas.move(self.ball, self.ball.x - oldx, self.ball.x - oldy)
+        else:
+            self.ball.x += self.ball.dx
+            self.ball.y += self.ball.dy
+            self.canvas.move(self.ball.ball, self.ball.dx, self.ball.dy)
 
-        #     if is_x_detected and is_y_detected:
-        #         break
+        self.debug_line = self.canvas.create_line(self.ball.x, self.ball.y, self.ball.x + self.ball.dx, self.ball.y + self.ball.dy, fill="#00ff00")
 
-        # if is_x_detected:
-        #     print("x block collision")
 
-        # if is_y_detected:
-        #     print("y block collision")
 
-        # class platform_wrap:
-        #     x = self.platform.x - self.platform.length / 2
-        #     y = self.platform.y - self.platform.thickness / 2
-        #     w = self.platform.length
-        #     h = self.platform.thickness
-
-        # # platform check
-        # if self.ball.check_collision_x(platform_wrap):
-        #     is_x_detected = True
-
-        # if self.ball.check_collision_y(platform_wrap):
-        #     is_y_detected = True
-
-        # # playground boundaries check
-
-        # if self.ball.check_boundaries_x(self.width):
-        #     is_x_detected = True
-
-        # if self.ball.check_boundaries_y(self.height):
-        #     is_y_detected = True
-
-        # if is_x_detected:
-        #     self.ball.dx *= -1
-
-        # if is_y_detected:
-        #     self.ball.dy *= -1
-
-        # self.ball.move()
-
-        self.root.after(50, self.gamecycle)
+        self.root.after(250, self.gamecycle)
 
     def hide_score(self):
         self.canvas.itemconfig(self.score_element, fill = self.background_color)
@@ -138,7 +213,7 @@ class Game():
     def start_new_game(self, event):
         self.platform = Platform(self.canvas, self.width, self.height)
         self.blocks = create_check_level(self.canvas, self.width, self.height)
-        #self.ball = Ball(self.canvas, self.platform.x , self.platform.y - 10, 10, 10)
+        self.ball = Ball(self.canvas, self.platform.x , self.platform.y - 20, 5, 10)
         self.root.bind('<Key>', self.platform.move_platform)
         self.show_score()
         self.gamecycle()
